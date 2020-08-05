2456060a619c5bd61556eb439cb02dd3962ea9d7
hexparvi <helena.m.parviainen@gmail.com>
2020-08-05 18:34:36 +0300

    progress(?) on jps

--- tiralab-pathfinding/src/main/java/tiralab/pathfinding/JPS.java
+++ tiralab-pathfinding/src/main/java/tiralab/pathfinding/JPS.java
@@ -18,14 +18,21 @@ import tiralab.pathfinding.domain.Node;
  * @author hexparvi
  */
 public class JPS {
-    //TODO
+    //TODO:
+    // 
+    
     private PriorityQueue<Node> unvisitedNodes = new PriorityQueue<>();
     private Set<String> visitedNodes = new HashSet<>();
+    private Heuristic heuristic = new Heuristic("euclidean");
     //is visitedNodes needed?
     
     public void run(Maze maze, Node start, Node end) {
-        //TODO, basically A* but neighbors & nodes added to visited are different?
+        //TODO, basically A* but neighbors & nodes added to unvisited are different?
+        start.setShortestDistance(0);
+        
         unvisitedNodes.add(start);
+        Node previousNode = null;
+        List<Node> successors = new ArrayList<>();
         
         while (unvisitedNodes.size() != 0) {
             Node currentNode = unvisitedNodes.remove();
@@ -35,25 +42,147 @@ public class JPS {
             }
             
             visitedNodes.add(currentNode.getName());
+            System.out.println("***Looking at node: " + currentNode.getName());
             
+            if (currentNode.equals(end)) {
+                System.out.println("Found end!");
+                break;
+            }
+            
+            if (previousNode != null) {
+                successors = successors(maze, start, end, currentNode, currentNode.getPreviousNode());
+            } else {
+                successors = maze.findNeighborsOfNode(currentNode);
+                for (Node neighbor : successors) {
+                    unvisitedNodes.add(neighbor);
+                    double newDistance = heuristic.estimateCost(currentNode, neighbor);
+                    neighbor.setShortestDistance(newDistance);
+                    neighbor.setPreviousNode(currentNode);
+                }
+            }
+            
+            
+//            for (Node node : successors) {
+//                //System.out.println("   successor: " + node.getName());
+//                System.out.println(">>Distance to this successor: " + node.getShortestDistance());
+//                System.out.println(">>Distance to current node(?): " + currentNode.getShortestDistance());
+//                System.out.println("");
+//                
+//                if (currentNode.getShortestDistance() < node.getShortestDistance() || !visitedNodes.contains(node.getName())) {
+//                    System.out.println("->>Successor not opened or distance is shorter, opening...");
+//                    if (currentNode.equals(start)) {
+//                        //shouldn't start node distance be logically 0?
+//                        double newDistance = heuristic.estimateCost(currentNode, node);
+//                        node.setShortestDistance(newDistance);
+//                        node.setPreviousNode(currentNode);
+//                    }
+//                    
+//                    System.out.print(node.getName() + " (came from [" + node.getPreviousNode().getName() + "]), ");
+//                    
+//                    
+//                    unvisitedNodes.add(node);
+//                }
+//            }
+            System.out.println("");
+            
+            //this is not right
+            previousNode = currentNode;
             //checkNeighbors(maze, currentNode, end);
         }
     }
     
-    private void successors(Maze maze, Node start, Node end, Node current) {
+    private List<Node> successors(Maze maze, Node start, Node end, Node current, Node parent) {
         List<Node> successors = new ArrayList<>();
-        List<Node> neighbors = maze.findNeighborsOfNode(current);
+        System.out.println("   Finding successors for node...");
+        
+        if (!maze.isWalkable(current.getX(), current.getY())) {
+            System.out.println("   This node is not walkable, aborting...");
+            return successors;
+        }
+        
+        List<Node> neighbors = prune(maze, parent, current, maze.findNeighborsOfNode(current));
+        
+        for (Node neighbor : neighbors) {
+            System.out.println("   Investigating neighbour " + neighbor.getName());
+            int dx = neighbor.getX() - current.getX();
+            int dy = neighbor.getY() - current.getY();
+//            System.out.println("dx: " + dx + ", dy :" + dy);
+//            System.out.println("neighbor x/y: " + neighbor.getX() + ", " + neighbor.getY());
+//            System.out.println("current x/y: " + current.getX() + ", " + current.getY());
+            Node jumpNode = jump(maze, current, end, current.getX() + dx, current.getY() + dy);
+            
+            if (jumpNode != null) {
+                
+//                double newDistance = current.getShortestDistance() + heuristic.estimateCost(current, jumpNode);
+//                jumpNode.setPreviousNode(current);
+//                jumpNode.setShortestDistance(newDistance);
+                
+                if (!visitedNodes.contains(jumpNode.getName()) || jumpNode.getShortestDistance() > current.getShortestDistance()) {
+                    unvisitedNodes.add(jumpNode);
+                }
+
+                successors.add(jumpNode);
+                System.out.println("***Jump successful, added node " + jumpNode.getName() + " to successor list***");
+                
+            } else {
+                System.out.println("   Jumped in direction of neighbor " + neighbor.getName() + ", but result was null.");
+            }
+        }
+        
+        return successors;
     }
     
-    private Node jump(Maze maze, Node current, Node neighbor, Node target) {
+    private Node jump(Maze maze, Node jumpedFrom, Node target, int x, int y) {
+        System.out.println("");
+        System.out.println("Jumped from node " + jumpedFrom.getName() + " to location " + x + ", " + y);
         Node jumpNode = null;
         
-        if (neighbor.isObstacle()) return null; //these should be pruned beforehand?
-        if (neighbor.equals(target)) return neighbor;
-        if (hasForcedNeighbors(neighbor, current, maze)) return neighbor;
+        if (!maze.isWalkable(x, y)) {
+            System.out.println("->This location [" + x + ", " + y + "] is not walkable! returning...");
+            return null;
+        }
+        Node jumpedTo = maze.getNodeAtPosition(x, y);
+        double newDistance = jumpedFrom.getShortestDistance() + heuristic.estimateCost(jumpedFrom, jumpedTo);
         
-        //if direction = diagonal jump horizontally and vertically from neighbor
-        //jump in direction from neighbor
+        jumpedTo.setPreviousNode(jumpedFrom);
+        jumpedTo.setShortestDistance(newDistance);
+        
+        //if (neighbor.isObstacle()) return null; //these should be pruned beforehand?
+        if (jumpedTo.equals(target)) {
+            System.out.println("->This jump location " + jumpedTo.getName() + " is the target node! returning...");
+            return jumpedTo;
+        }
+        if (hasForcedNeighbors(jumpedTo, jumpedFrom, maze)) {
+            System.out.println("->This jump location " + jumpedTo.getName() + " has forced neighbors, returning!");
+            return jumpedTo;
+        }
+        
+        int dx = x - jumpedFrom.getX();
+        int dy = y - jumpedFrom.getY();
+        
+        if (dx != 0 && dy != 0) {
+            System.out.println("***Movement is diagonal");
+            if (jump(maze, jumpedTo, target, (x + dx), (y)) != null) {
+                System.out.println("---Jumping horizontally");
+                return jumpedTo;
+            }
+            
+            if (jump(maze, jumpedTo, target, (x), (y + dy)) != null) {
+                System.out.println("---Jumping vertically");
+                return jumpedTo;
+            }
+        }
+        
+        System.out.println("---Jumping in same direction");
+        jumpNode = jump(maze, jumpedTo, target, (x + dx), (y + dy));
+        
+        if (jumpNode != null) {
+            
+            System.out.println("   jumpNode assignment result: " + jumpNode.getName());
+        } else {
+            System.out.println("   jumpNode assignment is null");
+        }
+        
         
         return jumpNode;
     }
@@ -93,11 +222,84 @@ public class JPS {
         return false;
     }
     
-    private void prune(Node parent, Node current, List<Node> neighbors) {
-        //TODO
+    //this is a nightmare
+    private List<Node> prune(Maze maze, Node parent, Node current, List<Node> neighbors) {
+        List<Node> prunedNeighbors = new ArrayList<>();
+        
+        int dx = current.getX() - parent.getX();
+        int dy = current.getY() - parent.getY();
+        
+        System.out.println("      dx: " + dx + ", dy :" + dy);
+//            System.out.println("parent x/y: " + parent.getX() + ", " + parent.getY());
+//            System.out.println("current x/y: " + current.getX() + ", " + current.getY());
+        
+        if (dx != 0 && dy != 0) {
+            if (maze.isWalkable(current.getX() + dx, current.getY() + dy)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() + dx, current.getY() + dy));
+            }
+            if (maze.isWalkable(current.getX(), current.getY() + dy)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX(), current.getY() + dy));
+            }
+            if (maze.isWalkable(current.getX() + dx, current.getY())) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() + dx, current.getY()));
+            }
+            if (!maze.isWalkable(current.getX() - dx, current.getY()) 
+                    && maze.isWalkable(current.getX() - dx,  current.getY() + dy)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() - dx,  current.getY() + dy));
+            }
+            if (!maze.isWalkable(current.getX(), current.getY() - dy) 
+                    && maze.isWalkable(current.getX() + dx, current.getY() - dy)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() + dx, current.getY() - dy));
+            }
+            
+        } else if (dx != 0) {
+            if (maze.isWalkable(current.getX() + dx, current.getY())) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() + dx, current.getY()));
+            }
+            
+            if (!maze.isWalkable(current.getX(), current.getY() + 1) 
+                    && maze.isWalkable(current.getX() + dx, current.getY() + 1)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() + dx, current.getY() + 1));
+            }
+            
+            if (!maze.isWalkable(current.getX(), current.getY() - 1) 
+                    && maze.isWalkable(current.getX() + dx, current.getY() - 1)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() + dx, current.getY() - 1));
+            }
+            
+        } else if (dy != 0) {
+            if (maze.isWalkable(current.getX(), current.getY() + dy)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX(), current.getY() + dy));
+            }
+            
+            if (!maze.isWalkable(current.getX() - 1, current.getY()) 
+                    && maze.isWalkable(current.getX() - 1, current.getY() + dy)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() - 1, current.getY() + dy));
+            }
+            
+            if (!maze.isWalkable(current.getX() + 1, current.getY()) 
+                    && maze.isWalkable(current.getX() + 1, current.getY() + dy)) {
+                prunedNeighbors.add(maze.getNodeAtPosition(current.getX() + 1, current.getY() + dy));
+            }
+        }
+        
+        return prunedNeighbors;
     }
     
-    private void movementDirection() {
+    public ArrayList<Node> getShortestRoute(Node start, Node end) {
+        ArrayList<Node> shortestRoute = new ArrayList<>();
         
+        shortestRoute.add(end);
+        Node currentNode = end;
+        
+        System.out.println("Printing found route: ");
+        System.out.println(currentNode.getName() + " ->");
+        while (!currentNode.equals(start)) {
+            System.out.println(currentNode.getPreviousNode().getName() + " -> ");
+            shortestRoute.add(currentNode.getPreviousNode());
+            currentNode = currentNode.getPreviousNode();
+        }
+        
+        return shortestRoute;
     }
 }
--- tiralab-pathfinding/src/main/java/tiralab/pathfinding/Main.java
+++ tiralab-pathfinding/src/main/java/tiralab/pathfinding/Main.java
@@ -17,7 +17,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        int[][] pixelArray = MyIO.readFromFile("src/testmazes/maze1.png");
+        int[][] pixelArray = MyIO.readFromFile("src/mazes/minicave.png");
 
         Maze maze = new Maze(pixelArray, pixelArray.length, pixelArray[0].length);
         maze.generateNodes();
@@ -27,20 +27,31 @@ public class Main {
 //        Node end = maze.getNodeAtPosition(9, 8);
 
         //start/end nodes for maze1
-        Node start = maze.getNodeAtPosition(484, 48);
-        Node end = maze.getNodeAtPosition(19, 1001);
+//        Node start = maze.getNodeAtPosition(484, 48);
+//        Node end = maze.getNodeAtPosition(19, 1001);
+        
+        //start/end nodes for cave1
+//        Node start = maze.getNodeAtPosition(484, 48);
+//        Node end = maze.getNodeAtPosition(344, 932);
+        
+        //start/end nodes for minicave
+        Node start = maze.getNodeAtPosition(1, 1);
+        Node end = maze.getNodeAtPosition(8, 8);
 
 //        Node start = maze.getNodeAtPosition(1,1);
 //        Node end = maze.getNodeAtPosition(4, 9);
         
         System.out.println("are nodes obstacles start/end: " + start.isObstacle() + "/" + end.isObstacle());
         
-        Astar dijkstra = new Astar();
-        Heuristic heuristic = new Heuristic("");
-        dijkstra.run(maze, start, end, heuristic);
+//        Astar dijkstra = new Astar();
+//        Heuristic heuristic = new Heuristic("");
+//        dijkstra.run(maze, start, end, heuristic);
+        
+        JPS2 jps = new JPS2();
+        jps.run(maze, start, end);
         
         
-        List<Node> path = dijkstra.getShortestRoute(start, end);
+        List<Node> path = jps.getShortestRoute(start, end);
         
         //add found path to pixelsArray
         for (Node node : path) {
